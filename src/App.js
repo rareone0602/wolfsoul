a7d286e5b7fc06a9b00cca8a9186619f8eae2047
rareone0602 <rareone0602@gmail.com>
2022-04-25 12:16:36 +0800

    Add restart and rickroll

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class Square extends React.Component {
     super(props);
   }
   render() {
-    let img_location = `wolfsoul_cropped_${Math.floor(this.props.index / 5)}_${this.props.index % 5}.jpg`;
+    let img_location = `wolfsoul_cropped_${Math.floor(this.props.index / 3)}_${this.props.index % 3}.jpg`;
     return (
       <img className="puzzle_img" src={img_location} onClick={() => this.props.onClick(this.props.index)}/>
     );
@@ -28,8 +28,6 @@ class Row extends React.Component {
         {this.renderSquare(this.props.indexs[0])}
         {this.renderSquare(this.props.indexs[1])}
         {this.renderSquare(this.props.indexs[2])}
-        {this.renderSquare(this.props.indexs[3])}
-        {this.renderSquare(this.props.indexs[4])}
       </div>
     )
     
@@ -46,12 +44,11 @@ class Board extends React.Component {
     return (
       <div>
         <div className="status">{status}</div>
-          {this.renderRow(this.props.permutation.slice( 0,  5))}
-          {this.renderRow(this.props.permutation.slice( 5, 10))}
-          {this.renderRow(this.props.permutation.slice(10, 15))}
-          {this.renderRow(this.props.permutation.slice(15, 20))}
-          {this.renderRow(this.props.permutation.slice(20, 25))}
-          {this.renderRow(this.props.permutation.slice(25, 30))}
+          {this.renderRow(this.props.permutation.slice( 0,  3))}
+          {this.renderRow(this.props.permutation.slice( 3,  6))}
+          {this.renderRow(this.props.permutation.slice( 6,  9))}
+          {this.renderRow(this.props.permutation.slice( 9, 12))}
+          {this.renderRow(this.props.permutation.slice(12, 15))}
       </div>
     );
   }
@@ -60,39 +57,53 @@ class Board extends React.Component {
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    let permutation = even_permutation(6 * 5);
     this.state = {
-      permutation: permutation
+      permutation: even_permutation(5 * 3)
     };
   }
 
   isValid(i, j) {
-    const ih = Math.floor(i / 5);
-    const jh = Math.floor(j / 5);
-    const iw = i % 5;
-    const jw = j % 5;
+    const ih = Math.floor(i / 3);
+    const jh = Math.floor(j / 3);
+    const iw = i % 3;
+    const jw = j % 3;
     return Math.abs(ih - jh) + Math.abs(iw - jw) == 1;
   }
 
   handleClick(i) {
     let permutation = this.state.permutation.slice();
     const toSwap = permutation.indexOf(i);
-    const unoccupied = permutation.indexOf(4 * 5 + 2);
+    const unoccupied = permutation.indexOf(3 * 3 + 1);
     if (this.isValid(toSwap, unoccupied)) {
-
-      console.log(permutation);
-      permutation[toSwap] = 4 * 5 + 2;
+      permutation[toSwap] = 3 * 3 + 1;
       permutation[unoccupied] = i;
       this.setState({
         permutation: permutation
       });
-      console.log(this.state);
+    }
+    let arraysEqual = (a, b) => {
+      if (a === b) return true;
+      if (a == null || b == null) return false;
+      if (a.length !== b.length) return false;
+      for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+      }
+      return true;
+    };
+    if (arraysEqual(permutation, [...Array(5 * 3).keys()])) {
+      window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ','rickroll').focus();
     }
   }
 
   render() {
     return (
+      
       <div className="game">
+        <div className='game-restart'>
+          <button onClick={()=>this.setState({permutation: even_permutation(5 * 3)})}>
+            Restart
+          </button>
+        </div>
         <div className="game-board">
           <Board permutation={this.state.permutation} onClick={i => this.handleClick(i)}/>
         </div>
